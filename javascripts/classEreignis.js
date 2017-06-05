acfff456e70ba2de418c08bb5d29304b66a803fa
TimHasert <timhasert@gmail.com>
2017-06-05 21:15:10 +0200

    Event-Umstellung

--- javascripts/classEreignis.js
+++ javascripts/classEreignis.js
@@ -1,7 +1,7 @@
 var EreignisArray = new Array;
 
 // Ereignis-Klasse
-function Ereignis (ID, name, type, standort, lat, lng, JahrV, JahrB) {
+function Ereignis (ID, name, type, standort, lat, lng, Jahr) {
     // TODO Deklaration vervollständigen, erst möglich nach Absprache mit Geistis
     this.id = ID;
     this.name = name;
@@ -9,8 +9,7 @@ function Ereignis (ID, name, type, standort, lat, lng, JahrV, JahrB) {
     this.standort = standort;
     this.latC = lat;
     this.lngC = lng;
-    this.time1 = JahrV;
-    this.time2 = JahrB;
+    this.time = Jahr;
 
     this.getID = function() {
       return this.id;
@@ -36,12 +35,8 @@ function Ereignis (ID, name, type, standort, lat, lng, JahrV, JahrB) {
       return this.lngC;
     };
 
-    this.getTime1 = function() {
-      return this.time1;
-    };
-
-    this.getTime2 = function() {
-      return this.time2;
+    this.getTime = function() {
+      return this.time;
     };
 
     this.getTList = function() {
@@ -56,19 +51,19 @@ function Ereignis (ID, name, type, standort, lat, lng, JahrV, JahrB) {
 // Ereignis-Array
 function InitializeE() {
 
-  EreignisArray.push(new Ereignis(0, "Bsp0", "Soziale Bewegung", "Erfurt", 50.930, 11.240, 1880, 1950));
-  EreignisArray.push(new Ereignis(1, "Bsp1", "Industrie 1", "Erfurt", 50.830, 11.840, 1850, 1920));
-  EreignisArray.push(new Ereignis(2, "Bsp2", "Industrie 3", "Eisenach", 51.330, 10.840, 1890, 1910));
-  EreignisArray.push(new Ereignis(3, "Bsp3", "Industrie 2", "Gotha", 50.730, 10.840, 1870, 1990));
+  EreignisArray.push(new Ereignis(0, "Bsp0", "Soziale Bewegung", "Erfurt", 50.930, 11.240, 1880));
+  EreignisArray.push(new Ereignis(1, "Bsp1", "Industrie 1", "Erfurt", 50.830, 11.840, 1850));
+  EreignisArray.push(new Ereignis(2, "Bsp2", "Industrie 3", "Eisenach", 51.330, 10.840, 1890));
+  EreignisArray.push(new Ereignis(3, "Bsp3", "Industrie 2", "Gotha", 50.730, 10.840, 1870));
 
-  EreignisArray.push(new Ereignis(4, "Bsp4", "Soziale Bewegung", "Jena", 50.784, 11.240, 1843, 1930));
-  EreignisArray.push(new Ereignis(5, "Bsp5", "Industrie 1", "Erfurt", 50.999, 11.840, 1820, 1980));
-  EreignisArray.push(new Ereignis(6, "Bsp6", "Industrie 3", "Suhl", 51.206, 10.840, 1770, 1980));
-  EreignisArray.push(new Ereignis(7, "Bsp7", "Industrie 2", "Nordhausen", 50.666, 10.867, 1790, 1990));
+  EreignisArray.push(new Ereignis(4, "Bsp4", "Soziale Bewegung", "Jena", 50.784, 11.240, 1843));
+  EreignisArray.push(new Ereignis(5, "Bsp5", "Industrie 1", "Erfurt", 50.999, 11.840, 1820));
+  EreignisArray.push(new Ereignis(6, "Bsp6", "Industrie 3", "Suhl", 51.206, 10.840, 1782));
+  EreignisArray.push(new Ereignis(7, "Bsp7", "Industrie 2", "Nordhausen", 50.666, 10.867, 1790));
 
-  EreignisArray.push(new Ereignis(8, "Bsp8", "Soziale Bewegung", "Erfurt", 50.930, 11.140, 1840, 1960));
-  EreignisArray.push(new Ereignis(9, "Bsp9", "Industrie 1", "Eisenach", 50.830, 11.120, 1710, 1980));
-  EreignisArray.push(new Ereignis(10, "Bsp10", "Industrie 3", "Weimar", 51.330, 10.560, 1710, 1910));
-  EreignisArray.push(new Ereignis(11, "Bsp11", "Industrie 2", "Erfurt", 50.730, 10.840, 1770, 1990));
+  EreignisArray.push(new Ereignis(8, "Bsp8", "Soziale Bewegung", "Erfurt", 50.930, 11.140, 1840));
+  EreignisArray.push(new Ereignis(9, "Bsp9", "Industrie 1", "Eisenach", 50.830, 11.120, 1790));
+  EreignisArray.push(new Ereignis(10, "Bsp10", "Industrie 3", "Weimar", 51.330, 10.560, 1820));
+  EreignisArray.push(new Ereignis(11, "Bsp11", "Industrie 2", "Erfurt", 50.730, 10.840, 1810));
 
 };
