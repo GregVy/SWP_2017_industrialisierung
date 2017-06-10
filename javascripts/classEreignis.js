b27781c86abbed15c24e516684ba12a6573a5609
TimHasert <timhasert@gmail.com>
2017-06-10 11:03:22 +0200

    Bild und Beschreibung hinzugefügt

--- javascripts/classEreignis.js
+++ javascripts/classEreignis.js
@@ -1,7 +1,8 @@
 var EreignisArray = new Array;
+var imageDirectory = "../images/"
 
 // Ereignis-Klasse
-function Ereignis (ID, name, type, standort, lat, lng, jahr, beschreibung) {
+function Ereignis (ID, name, type, standort, lat, lng, jahr, beschreibung, image) {
     this.id = ID;
     this.name = name;
     this.type = type;
@@ -10,6 +11,7 @@ function Ereignis (ID, name, type, standort, lat, lng, jahr, beschreibung) {
     this.lngC = lng;
     this.time = jahr;
     this.beschreibung = beschreibung;
+    this.image = image;
 
     this.getID = function() {
       return this.id;
@@ -43,6 +45,10 @@ function Ereignis (ID, name, type, standort, lat, lng, jahr, beschreibung) {
       return this.beschreibung;
     };
 
+    this.getImage = function() {
+      return imageDirectory + this.image;
+    };
+
     this.getTList = function() {
       return TemplateList(this.id, this.name, this.standort, this.type);
     };
@@ -51,17 +57,14 @@ function Ereignis (ID, name, type, standort, lat, lng, jahr, beschreibung) {
       return TemplateInfo(this.id, this.name, this.standort, this.type, this.time);
     };
 
-    this.getTDetail = function() {
-      return TemplateDetails(this.id, this.name);
-    };
 };
 
 // Ereignis-Array
 function InitializeE() {
-  EreignisArray.push(new Ereignis(0, "Bsp0", "Soziale Bewegung", "Erfurt", 50.930, 11.240, 1880));
-  EreignisArray.push(new Ereignis(1, "Bsp1", "Produktionsgüterindustrie", "Erfurt", 50.830, 11.840, 1850));
-  EreignisArray.push(new Ereignis(2, "Bsp2", "Investitionsgüterindustrie", "Eisenach", 51.330, 10.840, 1890));
-  EreignisArray.push(new Ereignis(3, "Bsp3", "Verbrauchsgüterindustrie", "Gotha", 50.730, 10.840, 1870));
-  EreignisArray.push(new Ereignis(4, "Bsp4", "Genußmittelindustrie", "Jena", 50.784, 11.240, 1843));
-  EreignisArray.push(new Ereignis(5, "Bsp5", "Kommunikationsindustrie", "Erfurt", 50.999, 11.840, 1820));
+  EreignisArray.push(new Ereignis(0, "Bsp0", "Soziale Bewegung", "Erfurt", 50.930, 11.240, 1880, "Ich bin ein Beschreibungstext", "Platzhalter.png"));
+  EreignisArray.push(new Ereignis(1, "Bsp1", "Produktionsgüterindustrie", "Erfurt", 50.830, 11.840, 1850, "Ich bin ein Beschreibungstext", "Platzhalter.png"));
+  EreignisArray.push(new Ereignis(2, "Bsp2", "Investitionsgüterindustrie", "Eisenach", 51.330, 10.840, 1890, "Ich bin ein Beschreibungstext", "Platzhalter.png"));
+  EreignisArray.push(new Ereignis(3, "Bsp3", "Verbrauchsgüterindustrie", "Gotha", 50.730, 10.840, 1870, "Ich bin ein Beschreibungstext", "Platzhalter.png"));
+  EreignisArray.push(new Ereignis(4, "Bsp4", "Genußmittelindustrie", "Jena", 50.784, 11.240, 1843, "Ich bin ein Beschreibungstext", "Platzhalter.png"));
+  EreignisArray.push(new Ereignis(5, "Bsp5", "Kommunikationsindustrie", "Erfurt", 50.999, 11.840, 1820, "Ich bin ein Beschreibungstext", "Platzhalter.png"));
 };
